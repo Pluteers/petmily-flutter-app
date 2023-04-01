f2367cfb5c40587fcb9285e3cfa49f0a8d540ca2
yujinkim1 <yujin7621@me.com>
2023-04-01 19:02:40 +0900

    modified: Format on save mode

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,25 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: true, fontFamily: "PretendardVariation",),
+      theme: ThemeData(
+        useMaterial3: true,
+        fontFamily: "PretendardVariation",
+      ),
       home: Scaffold(
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              testVariableFonts("이런 폰트를 사용할 수 있다고? 대박아니야?", 100.0, 100.0, 100.0),
-              testVariableFonts("이런 폰트를 사용할 수 있다고? 대박아니야?", 300.0, 300.0, 300.0),
-              testVariableFonts("이런 폰트를 사용할 수 있다고? 대박아니야?", 500.0, 500.0, 500.0),
-              testVariableFonts("이런 폰트를 사용할 수 있다고? 대박아니야?", 700.0, 700.0, 700.0),
-              testVariableFonts("이런 폰트를 사용할 수 있다고? 대박아니야?", 900.0, 900.0, 900.0),
+              testVariableFonts(
+                  "이런 폰트를 사용할 수 있다고? 대박아니야?", 100.0, 100.0, 100.0),
+              testVariableFonts(
+                  "이런 폰트를 사용할 수 있다고? 대박아니야?", 300.0, 300.0, 300.0),
+              testVariableFonts(
+                  "이런 폰트를 사용할 수 있다고? 대박아니야?", 500.0, 500.0, 500.0),
+              testVariableFonts(
+                  "이런 폰트를 사용할 수 있다고? 대박아니야?", 700.0, 700.0, 700.0),
+              testVariableFonts(
+                  "이런 폰트를 사용할 수 있다고? 대박아니야?", 900.0, 900.0, 900.0),
             ],
           ),
         ),
@@ -33,6 +41,14 @@ class MainApp extends StatelessWidget {
 Widget testVariableFonts(String text, double wght, double wdth, double grad) {
   return Text(
     text,
-    style: TextStyle(fontFamily: "PretendardVariation", fontSize: 20.0, fontVariations: <FontVariation>[FontVariation('wght', wght), FontVariation("wdth", wdth), FontVariation("GRAD", grad)],),
+    style: TextStyle(
+      fontFamily: "PretendardVariation",
+      fontSize: 20.0,
+      fontVariations: <FontVariation>[
+        FontVariation('wght', wght),
+        FontVariation("wdth", wdth),
+        FontVariation("GRAD", grad),
+      ],
+    ),
   );
 }
