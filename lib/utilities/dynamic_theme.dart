c3bb2dfb7057531f7fec5758b4714b7690605b0c
yujinkim1 <yujin7621@me.com>
2023-04-04 23:03:05 +0900

    modified: Seed color

--- lib/utilities/dynamic_theme.dart
+++ lib/utilities/dynamic_theme.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 
-class DynamicTheme {
-  static const _seedColor = Color(0XFFF6F1C4);
+const _seedColor = Color(0XFFBDB76B);
 
+class DynamicTheme {
   static ThemeData lightTheme(ColorScheme? lightColorScheme) {
-    ColorScheme defaultLightColorScheme = lightColorScheme ?? ColorScheme.fromSeed(seedColor: _seedColor, brightness: Brightness.light);
+    ColorScheme defaultLightColorScheme = lightColorScheme ??
+        ColorScheme.fromSeed(
+            seedColor: _seedColor, brightness: Brightness.light);
     return ThemeData(
       colorScheme: defaultLightColorScheme,
       useMaterial3: true,
       fontFamily: "PretendardVariation",
-    );  
+    );
   }
 
   static ThemeData darkTheme(ColorScheme? darkColorScheme) {
-    ColorScheme defaultDarkColorScheme = darkColorScheme ?? ColorScheme.fromSeed(seedColor: _seedColor, brightness: Brightness.dark);
+    ColorScheme defaultDarkColorScheme = darkColorScheme ??
+        ColorScheme.fromSeed(
+            seedColor: _seedColor, brightness: Brightness.dark);
     return ThemeData(
       colorScheme: defaultDarkColorScheme,
       useMaterial3: true,
