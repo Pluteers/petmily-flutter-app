6085bacb7ffb73073f4b3801ab57115ea6ca18b8
devpark435 <devpark435@gmail.com>
2023-04-11 13:10:22 +0900

    add : category grid ontap method

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:petmily/screens/channel.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -33,35 +34,45 @@ class HomeScreen extends StatelessWidget {
             children: List.generate(imageDataList.length, (index) {
               return Padding(
                 padding: const EdgeInsets.all(10.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    image: DecorationImage(
-                      image: AssetImage(
-                          imageDataList[index].imagePath), // 이미지 파일 경로
-                      fit: BoxFit.cover, // 이미지 채우는 방법
+                child: GestureDetector(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      image: DecorationImage(
+                        image: AssetImage(
+                            imageDataList[index].imagePath), // 이미지 파일 경로
+                        fit: BoxFit.cover, // 이미지 채우는 방법
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                            width: _width,
+                            padding: EdgeInsets.all(5),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: dynamicColor.primary,
+                            ),
+                            child: Text(
+                              imageDataList[index].name,
+                              style: TextStyle(color: dynamicColor.onPrimary),
+                            )),
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Text(imageDataList[index].description),
+                        ),
+                      ],
                     ),
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                          width: _width,
-                          padding: EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color: dynamicColor.primary,
-                          ),
-                          child: Text(
-                            imageDataList[index].name,
-                            style: TextStyle(color: dynamicColor.onPrimary),
-                          )),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Text(imageDataList[index].description),
-                      ),
-                    ],
-                  ),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ChannelScreen(
+                                  channelId: imageDataList[index].name,
+                                )));
+                  },
                 ),
               );
             }),
