6d2718ff65a54a7c35d061c4415a1a340220566d
yujinkim1 <yujin7621@me.com>
2023-04-11 19:57:10 +0900

    modified(#2): Aligning required widgets

--- lib/screens/petmily_gpt_screen.dart
+++ lib/screens/petmily_gpt_screen.dart
@@ -8,8 +8,6 @@ class PetmilyGPTScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dynamicColor = Theme.of(context).colorScheme;
-    final width = MediaQuery.of(context).size.width;
-    final height = MediaQuery.of(context).size.height;
     if (Platform.isIOS) {
       ///TODO: 쿠퍼티노 디자인을 적용해야 합니다.
       return const Scaffold(
@@ -18,15 +16,17 @@ class PetmilyGPTScreen extends StatelessWidget {
     } else {
       ///머티리얼 디자인을 적용합니다.
       return Scaffold(
-        backgroundColor: dynamicColor.surfaceVariant,
+        backgroundColor: dynamicColor.surface,
+        resizeToAvoidBottomInset: false,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               HeaderContentWidget(
                 color: dynamicColor.surfaceVariant,
               ),
-              const BodyContentWidget()
+              const TipContentWidget(),
+              const BodyContentWidget(),
             ],
           ),
         ),
@@ -44,6 +44,9 @@ class PetmilyGPTScreen extends StatelessWidget {
             ],
           ),
         ),
+        persistentFooterButtons: const <Widget>[
+          //TODO: Form field를 적용할 수 있도록 시도해봅니다.
+        ],
       );
     }
   }
@@ -57,15 +60,57 @@ class HeaderContentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dynamicColor = Theme.of(context).colorScheme;
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(color: color),
-      child: Center(
-        child: VariableText(
-          value: "GPT",
-          color: dynamicColor.primary,
-          size: 40.0,
-          wght: 700.0,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: VariableText(
+              value: "대화하기",
+              color: dynamicColor.primary,
+              size: 40.0,
+              wght: 300.0,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TipContentWidget extends StatelessWidget {
+  const TipContentWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dynamicColor = Theme.of(context).colorScheme;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        width: 370.0,
+        height: 35,
+        decoration: BoxDecoration(
+          color: dynamicColor.tertiaryContainer,
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.tips_and_updates,
+              color: dynamicColor.tertiary,
+              size: 20.0,
+            ),
+            VariableText(
+              value: "부적절한 단어나 다른 주제의 질문은 답변하지 않습니다.",
+              color: dynamicColor.tertiary,
+              size: 15.0,
+              wght: 500.0,
+            ),
+          ],
         ),
       ),
     );
@@ -92,8 +137,19 @@ class _BodyContentWidgetState extends State<BodyContentWidget> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: [inputFormField(), sendButton()],
+      child: Column(
+        children: [
+          SingleChildScrollView(
+            child: inputOutputListView(),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              inputFormField(),
+              sendButton(),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -104,13 +160,13 @@ class _BodyContentWidgetState extends State<BodyContentWidget> {
     super.dispose();
   }
 
-  Expanded inputFormField() {
+  Widget inputFormField() {
     return Expanded(
       child: TextFormField(
         textCapitalization: TextCapitalization.sentences,
         controller: _inputTextController,
         decoration: InputDecoration(
-          fillColor: Theme.of(context).colorScheme.inverseSurface,
+          fillColor: Theme.of(context).colorScheme.surfaceVariant,
           filled: true,
           hintText: "메세지를 입력하세요.",
           border: InputBorder.none,
@@ -130,8 +186,30 @@ class _BodyContentWidgetState extends State<BodyContentWidget> {
         onPressed: () {
           //TODO: inputTextController.text의 값을 감지하고, 전송 및 초기화를 담당해야 합니다.
         },
-        icon: const Icon(Icons.subdirectory_arrow_left_rounded),
+        icon: Icon(
+          Icons.subdirectory_arrow_left_rounded,
+          color: Theme.of(context).colorScheme.primary,
+        ),
       ),
+    );
+  }
+
+  Widget inputOutputListView() {
+    return ListView(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      children: const <Widget>[
+        SizedBox(
+          height: 380.0,
+          child: Center(
+            child: VariableText(
+              value: "대화를 시작하면 내용이 나타납니다.",
+              size: 12.0,
+              wght: 300.0,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
