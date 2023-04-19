ebd7dc1bdfc0ea48dd6ca1763d308a2a4ba94492
yujinkim1 <yujin7621@me.com>
2023-04-19 15:58:34 +0900

    modified(#12): Use const keyword

--- lib/screens/signin/initial_screen.dart
+++ lib/screens/signin/initial_screen.dart
@@ -1,49 +1,60 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:petmily/providers/account_validator.dart';
+import 'package:petmily/widgets/variable_text.dart';
 import 'package:provider/provider.dart';
 import 'package:petmily/screens/signin/local_signin_screen.dart';
 import 'package:petmily/screens/signup/signup_screen.dart';
-import 'package:petmily/services/login_viewmodel.dart';
 
-class PetmilySigninScreen extends StatelessWidget {
-  const PetmilySigninScreen({Key? key}) : super(key: key);
+class InitialScreen extends StatelessWidget {
+  const InitialScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Color surfaceColor = Theme.of(context).colorScheme.surface;
+
     return ChangeNotifierProvider(
-      create: (context) => LoginViewModel(),
+      create: (context) => AccountValidator(),
       child: Scaffold(
-        appBar: AppBar(title: Text('Login')),
-        body: _LoginView(),
+        backgroundColor: surfaceColor,
+        body: const InitialWidget(),
       ),
     );
   }
 }
 
-class _LoginView extends StatelessWidget {
+class InitialWidget extends StatefulWidget {
+  const InitialWidget({super.key});
+
+  @override
+  State<InitialWidget> createState() => _InitialWidgetState();
+}
+
+class _InitialWidgetState extends State<InitialWidget> {
   @override
   Widget build(BuildContext context) {
-    final viewModel = Provider.of<LoginViewModel>(context);
-
+    //final accountValidator = Provider.of<AccountValidator>(context);
+    Color primaryColor = Theme.of(context).colorScheme.primary;
     return Container(
       alignment: Alignment.center,
       child: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text(
-              'Petmiliy',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 45.0),
+            VariableText(
+              value: "Petmiliy",
+              size: 45.0,
+              wght: 700.0,
+              color: primaryColor,
             ),
-            const Text(
-              '우리가 만드는 반려동물 캔버스',
-              style: TextStyle(fontSize: 22.0),
+            VariableText(
+              value: "우리가 만드는 반려동물 캔버스",
+              size: 20.0,
+              wght: 300.0,
+              color: primaryColor,
             ),
-            const SizedBox(
-              height: 60.0,
-            ),
+            const SizedBox(height: 60.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -52,7 +63,7 @@ class _LoginView extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const LoginPetmiliyView()),
+                          builder: (context) => const LocalSigninScreen()),
                     );
                   },
                   icon: Image.asset(
@@ -66,7 +77,7 @@ class _LoginView extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const LoginPetmiliyView()),
+                          builder: (context) => const LocalSigninScreen()),
                     );
                   },
                   icon: Image.asset(
@@ -80,7 +91,7 @@ class _LoginView extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const LoginPetmiliyView()),
+                          builder: (context) => const LocalSigninScreen()),
                     );
                   },
                   icon: Image.asset(
@@ -91,27 +102,35 @@ class _LoginView extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             ElevatedButton(
-              child: Text("Petmiliy 계정으로 접속", style: TextStyle(fontSize: 21)),
+              child: const VariableText(
+                value: "Petmiliy계정으로 접속",
+                size: 20.0,
+                wght: 400.0,
+              ),
               onPressed: () {
-                // 회원가입 화면으로 이동
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => LoginPetmiliyView()),
+                  MaterialPageRoute(
+                    builder: (context) => const LocalSigninScreen(),
+                  ),
                 );
               },
             ),
-            SizedBox(
-              height: 20.0,
-            ),
+            const SizedBox(height: 20.0),
             ElevatedButton(
-              child: Text("Petmiliy 계정 만들기", style: TextStyle(fontSize: 21)),
+              child: const VariableText(
+                value: "Petmiliy계정 만들기",
+                size: 20.0,
+                wght: 400.0,
+              ),
               onPressed: () {
-                // 회원가입 화면으로 이동
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => SignupView()),
+                  MaterialPageRoute(
+                    builder: (context) => const SignupScreen(),
+                  ),
                 );
               },
             ),
