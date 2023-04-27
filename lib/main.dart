474e463c9a79bcce6c26f636a57465e89e1f69c3
devpark435 <devpark435@gmail.com>
2023-04-27 12:34:43 +0900

    fix : dynamicColor Error

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:dynamic_color/dynamic_color.dart';
-import 'package:petmily/screens/home.dart';
 import 'package:petmily/utilities/dynamic_theme.dart';
 
 void main() {
@@ -14,45 +13,45 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DynamicColorBuilder(
-      builder: (ColorScheme? lightColorScheme, ColorScheme? darkColorScheme) => MaterialApp(
-          theme: DynamicTheme.lightTheme(lightColorScheme),
-          darkTheme: DynamicTheme.darkTheme(darkColorScheme),
-          themeMode: ThemeMode.light,
-          home: const HomeScreen()
-          // Scaffold(
-          //   body: Center(
-          //     child: Column(
-          //       mainAxisAlignment: MainAxisAlignment.center,
-          //       children: [
-          //         testVariableFonts(
-          //             "이런 폰트를 사용할 수 있다고? 대박아니야?", 100.0, 100.0, 100.0),
-          //         testVariableFonts(
-          //             "이런 폰트를 사용할 수 있다고? 대박아니야?", 300.0, 300.0, 300.0),
-          //         testVariableFonts(
-          //             "이런 폰트를 사용할 수 있다고? 대박아니야?", 500.0, 500.0, 500.0),
-          //         testVariableFonts(
-          //             "이런 폰트를 사용할 수 있다고? 대박아니야?", 700.0, 700.0, 700.0),
-          //         testVariableFonts(
-          //             "이런 폰트를 사용할 수 있다고? 대박아니야?", 900.0, 900.0, 900.0),
-          //       ],
-          //     ),
-          //   ),
-          //   bottomNavigationBar: BottomAppBar(
-          //     color: Theme.of(context).colorScheme.surface,
-          //     child: Row(
-          //       children: [
-          //         IconButton(
-          //             onPressed: () {}, icon: const Icon(Icons.circle_rounded)),
-          //         const Spacer(),
-          //         IconButton(
-          //             onPressed: () {}, icon: const Icon(Icons.circle_rounded)),
-          //         IconButton(
-          //             onPressed: () {}, icon: const Icon(Icons.circle_rounded)),
-          //       ],
-          //     ),
-          //   ),
-          // ),
+      builder: (ColorScheme? lightColorScheme, ColorScheme? darkColorScheme) =>
+          MaterialApp(
+        theme: DynamicTheme.lightTheme(lightColorScheme),
+        darkTheme: DynamicTheme.darkTheme(darkColorScheme),
+        themeMode: ThemeMode.light,
+        home: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                testVariableFonts(
+                    "이런 폰트를 사용할 수 있다고? 대박아니야?", 100.0, 100.0, 100.0),
+                testVariableFonts(
+                    "이런 폰트를 사용할 수 있다고? 대박아니야?", 300.0, 300.0, 300.0),
+                testVariableFonts(
+                    "이런 폰트를 사용할 수 있다고? 대박아니야?", 500.0, 500.0, 500.0),
+                testVariableFonts(
+                    "이런 폰트를 사용할 수 있다고? 대박아니야?", 700.0, 700.0, 700.0),
+                testVariableFonts(
+                    "이런 폰트를 사용할 수 있다고? 대박아니야?", 900.0, 900.0, 900.0),
+              ],
+            ),
           ),
+          bottomNavigationBar: BottomAppBar(
+            color: Theme.of(context).colorScheme.surface,
+            child: Row(
+              children: [
+                IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.circle_rounded)),
+                const Spacer(),
+                IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.circle_rounded)),
+                IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.circle_rounded)),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
