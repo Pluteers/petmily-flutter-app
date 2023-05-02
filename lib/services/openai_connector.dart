d68616e450156f997f010bca7405cffebd0a7fe7
yujinkim1 <yujin7621@me.com>
2023-05-02 18:53:26 +0900

    feat(#4): Input filtering for gpt model

--- lib/services/openai_connector.dart
+++ lib/services/openai_connector.dart
@@ -1,21 +1,19 @@
+import "dart:developer";
 import "dart:io";
 import "dart:convert";
-import "package:flutter/foundation.dart";
 import "package:http/http.dart" as http;
+import "package:petmily/providers/chat_provider.dart";
 import "package:petmily/utilities/constants.dart";
 
-class OpenaiConnector {
-  @protected
-  String url = Constants.openai;
-  @protected
-  String model = Constants.model;
-  //TODO: env 파일을 구성해서 예민한 값을 저장하고 사용할 수 있도록 할 예정입니다.
-  @protected
-  String token = "{MY_OPEN_API_KEY}";
+const String initalPromptValue = "반려동물과 관련된 질문에 한 문장으로만 짧게 답변하세요.";
 
-  String get _url => url;
-  String get _model => model;
-  String get _token => token;
+class OpenaiConnector {
+  String get _domain => Constants.openai;
+  String get _model => Constants.model;
+
+  ///- Env 파일을 구성하고 민감한 값을 가리고 사용할 수 있게끔 작업해야함.
+  ///String get _token => Env.get("OPENAI_SECRET_KEY")!;
+  String get _token => "{YOUR_OPENAI_API_KEY_HERE}";
 
   ///- 사용자가 작성한 문자열을 OpenAI 모델에 전송하는 비동기 함수입니다.
   ///- `[String] input`: 사용자가 작성한 문자열을 전달 받는 파라미터입니다.
@@ -25,32 +23,42 @@ class OpenaiConnector {
     if (input.isNotEmpty) {
       try {
         var response = await http.post(
-          Uri.parse(_url + _model),
+          Uri.parse("$_domain/chat/completions"),
           headers: {
             "Content-Type": "application/json",
-            "Authorization": _token,
+            "Authorization": "Bearer $_token",
           },
           body: jsonEncode({
-            "prompt": input,
+            "model": _model,
+            "messages": [
+              {"role": "system", "content": initalPromptValue},
+              {"role": "user", "content": input}
+            ],
             "temperature": 0.5,
-            "max_tokens": 120,
+            "max_tokens": 100,
             "top_p": 1,
             "frequency_penalty": 0,
             "presence_penalty": 0,
           }),
         );
         if (response.statusCode == 200) {
-          Map<String, dynamic> responsedData =
+          Map<String, dynamic> responseData =
               jsonDecode(utf8.decode(response.bodyBytes));
-          String output = responsedData["choies"][0]["text"].toString().trim();
+          if (responseData["error"] != null) {
+            throw HttpException(responseData["error"]["message"]);
+          }
+          String output = responseData['choices'][0]['message']['content']
+              .toString()
+              .trim();
+          log(output);
           return output;
-        } else if (response.statusCode == 401) {
-          String errorMessage = "Invalid a token";
+        } else {
+          Map<String, dynamic> responseData =
+              jsonDecode(utf8.decode(response.bodyBytes));
+          log("$responseData");
+          String errorMessage = "오류가 발생했습니다. 다시 시도해주세요.";
           return errorMessage;
-        } else if (response.statusCode == 402) {
-          String errorMessage = "Invalid a token";
-          return errorMessage;
-        } else {}
+        }
       } on SocketException {
         String errorMessage = "네트워크가 원활하지 않습니다. 다시 시도해주세요.";
         return errorMessage;
@@ -58,6 +66,5 @@ class OpenaiConnector {
     } else {
       return emptyOutput;
     }
-    return emptyOutput;
   }
 }
