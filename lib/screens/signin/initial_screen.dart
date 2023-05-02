25ef5a923d0eccb741e77f7be462ba9300f8fba5
yujinkim1 <yujin7621@me.com>
2023-05-02 23:48:11 +0900

    modified: Login with kakao sdk

--- lib/screens/signin/initial_screen.dart
+++ lib/screens/signin/initial_screen.dart
@@ -1,8 +1,8 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:petmily/providers/local_authenticator.dart';
+import 'package:petmily/providers/social_provider.dart';
 import 'package:petmily/screens/signin/local_signin_screen.dart';
-import 'package:petmily/screens/signup/signup_screen.dart';
 import 'package:petmily/widgets/variable_text.dart';
 
 class InitialScreen extends StatelessWidget {
@@ -11,9 +11,8 @@ class InitialScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Color surfaceColor = Theme.of(context).colorScheme.surface;
-
     return ChangeNotifierProvider(
-      create: (context) => LocalAuthenticator(),
+      create: (context) => SocialProvider(),
       child: Scaffold(
         backgroundColor: surfaceColor,
         body: const InitialContentWidget(),
@@ -35,6 +34,10 @@ class _InitialContentWidgetState extends State<InitialContentWidget> {
   Image? naverIcon;
   Image? kakaoIcon;
 
+  bool isKakaoTalkInstalled = false;
+
+  late SocialProvider socialProvider;
+
   @override
   void initState() {
     super.initState();
@@ -42,6 +45,7 @@ class _InitialContentWidgetState extends State<InitialContentWidget> {
 
   @override
   void didChangeDependencies() {
+    socialProvider = Provider.of<SocialProvider>(context);
     primaryColor = Theme.of(context).colorScheme.primary;
     googleIcon = Image.asset("assets/images/ic_google.png");
     naverIcon = Image.asset("assets/images/ic_naver.png");
@@ -71,13 +75,13 @@ class _InitialContentWidgetState extends State<InitialContentWidget> {
               wght: 300.0,
               color: primaryColor,
             ),
-            const SizedBox(height: 60.0),
+            const SizedBox(height: 160.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IconButton(
                   constraints:
-                      const BoxConstraints(maxWidth: 40.0, maxHeight: 40.0),
+                      const BoxConstraints(maxWidth: 50.0, maxHeight: 50.0),
                   onPressed: () {
                     Navigator.push(
                       context,
@@ -88,9 +92,10 @@ class _InitialContentWidgetState extends State<InitialContentWidget> {
                   },
                   icon: googleIcon!,
                 ),
+                const SizedBox(width: 20.0),
                 IconButton(
                   constraints:
-                      const BoxConstraints(maxWidth: 40.0, maxHeight: 40.0),
+                      const BoxConstraints(maxWidth: 50.0, maxHeight: 50.0),
                   onPressed: () {
                     Navigator.push(
                       context,
@@ -101,22 +106,22 @@ class _InitialContentWidgetState extends State<InitialContentWidget> {
                   },
                   icon: naverIcon!,
                 ),
+                const SizedBox(width: 20.0),
                 IconButton(
                   constraints:
-                      const BoxConstraints(maxWidth: 40.0, maxHeight: 40.0),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const LocalSigninScreen(),
-                      ),
-                    );
+                      const BoxConstraints(maxWidth: 50.0, maxHeight: 50.0),
+                  onPressed: () async {
+                    await socialProvider.login();
+                    if (socialProvider.authenticateState ==
+                        AuthenticateState.success) {
+                      log("카카오 로그인 성공");
+                    }
                   },
                   icon: kakaoIcon!,
                 ),
               ],
             ),
-            const SizedBox(height: 20.0),
+            const SizedBox(height: 60.0),
             OutlinedButton(
               style: ButtonStyle(
                 shape: MaterialStateProperty.all(
@@ -126,7 +131,7 @@ class _InitialContentWidgetState extends State<InitialContentWidget> {
                 ),
               ),
               child: const VariableText(
-                value: "Petmiliy 계정으로 로그인",
+                value: "Petmiliy 계정으로 시작하기",
                 size: 15.0,
                 wght: 400.0,
               ),
@@ -135,29 +140,6 @@ class _InitialContentWidgetState extends State<InitialContentWidget> {
                   context,
                   MaterialPageRoute(
                     builder: (context) => const LocalSigninScreen(),
-                  ),
-                );
-              },
-            ),
-            const SizedBox(height: 20.0),
-            OutlinedButton(
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                ),
-              ),
-              child: const VariableText(
-                value: "Petmiliy 계정 생성하기",
-                size: 15.0,
-                wght: 400.0,
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SignupScreen(),
                   ),
                 );
               },
