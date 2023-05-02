85381f5efa86ed5c1097b48ef063e1d05ea656f4
yujinkim1 <yujin7621@me.com>
2023-05-02 23:47:26 +0900

    modified: Commented google authenticator

--- lib/providers/google_authenticator.dart
+++ lib/providers/google_authenticator.dart
@@ -1,91 +1,91 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/foundation.dart';
 
-enum GoogleStatus { loading, canceled, error, success, idle }
+// enum GoogleStatus { loading, canceled, error, success, idle }
 
-class GoogleAuthenticator extends ChangeNotifier {
-  @protected
-  final String _email = "";
-  @protected
-  final String _password = "";
+// class GoogleAuthenticator extends ChangeNotifier {
+//   @protected
+//   final String _email = "";
+//   @protected
+//   final String _password = "";
 
-  String get email => _email;
-  String get password => _password;
+//   String get email => _email;
+//   String get password => _password;
 
-  User? currentUser() {
-    return FirebaseAuth.instance.currentUser;
-  }
+//   User? currentUser() {
+//     return FirebaseAuth.instance.currentUser;
+//   }
 
-  void signUp({
-    required String email, // 이메일
-    required String password, // 비밀번호
-    required Function() onSuccess, // 가입 성공시 호출되는 함수
-    required Function(String err) onError, // 에러 발생시 호출되는 함수
-  }) async {
-    // 회원가입
-    // 이메일 및 비밀번호 입력 여부 확인
-    if (email.isEmpty) {
-      onError("이메일을 입력해 주세요.");
-      return;
-    } else if (password.isEmpty) {
-      onError("비밀번호를 입력해 주세요.");
-      return;
-    }
+//   void signUp({
+//     required String email, // 이메일
+//     required String password, // 비밀번호
+//     required Function() onSuccess, // 가입 성공시 호출되는 함수
+//     required Function(String err) onError, // 에러 발생시 호출되는 함수
+//   }) async {
+//     // 회원가입
+//     // 이메일 및 비밀번호 입력 여부 확인
+//     if (email.isEmpty) {
+//       onError("이메일을 입력해 주세요.");
+//       return;
+//     } else if (password.isEmpty) {
+//       onError("비밀번호를 입력해 주세요.");
+//       return;
+//     }
 
-    // firebase auth 회원 가입
-    try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
+//     // firebase auth 회원 가입
+//     try {
+//       await FirebaseAuth.instance.createUserWithEmailAndPassword(
+//         email: email,
+//         password: password,
+//       );
 
-      // 성공 함수 호출
-      onSuccess();
-    } on FirebaseAuthException catch (e) {
-      // Firebase auth 에러 발생
-      onError(e.message!);
-    } catch (e) {
-      // Firebase auth 이외의 에러 발생
-      onError(e.toString());
-    }
-  }
+//       // 성공 함수 호출
+//       onSuccess();
+//     } on FirebaseAuthException catch (e) {
+//       // Firebase auth 에러 발생
+//       onError(e.message!);
+//     } catch (e) {
+//       // Firebase auth 이외의 에러 발생
+//       onError(e.toString());
+//     }
+//   }
 
-  void signIn({
-    required String email, // 이메일
-    required String password, // 비밀번호
-    required Function() onSuccess, // 로그인 성공시 호출되는 함수
-    required Function(String err) onError, // 에러 발생시 호출되는 함수
-  }) async {
-    // 로그인
-    if (email.isEmpty) {
-      onError('이메일을 입력해주세요.');
-      return;
-    } else if (password.isEmpty) {
-      onError('비밀번호를 입력해주세요.');
-      return;
-    }
+//   void signIn({
+//     required String email, // 이메일
+//     required String password, // 비밀번호
+//     required Function() onSuccess, // 로그인 성공시 호출되는 함수
+//     required Function(String err) onError, // 에러 발생시 호출되는 함수
+//   }) async {
+//     // 로그인
+//     if (email.isEmpty) {
+//       onError('이메일을 입력해주세요.');
+//       return;
+//     } else if (password.isEmpty) {
+//       onError('비밀번호를 입력해주세요.');
+//       return;
+//     }
 
-    // 로그인 시도
-    try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
+//     // 로그인 시도
+//     try {
+//       await FirebaseAuth.instance.signInWithEmailAndPassword(
+//         email: email,
+//         password: password,
+//       );
 
-      onSuccess(); // 성공 함수 호출
-      notifyListeners(); // 로그인 상태 변경 알림
-    } on FirebaseAuthException catch (e) {
-      // firebase auth 에러 발생
-      onError(e.message!);
-    } catch (e) {
-      // Firebase auth 이외의 에러 발생
-      onError(e.toString());
-    }
-  }
+//       onSuccess(); // 성공 함수 호출
+//       notifyListeners(); // 로그인 상태 변경 알림
+//     } on FirebaseAuthException catch (e) {
+//       // firebase auth 에러 발생
+//       onError(e.message!);
+//     } catch (e) {
+//       // Firebase auth 이외의 에러 발생
+//       onError(e.toString());
+//     }
+//   }
 
-  void signOut() async {
-    // 로그아웃
-    await FirebaseAuth.instance.signOut();
-    notifyListeners(); // 로그인 상태 변경 알림
-  }
-}
+//   void signOut() async {
+//     // 로그아웃
+//     await FirebaseAuth.instance.signOut();
+//     notifyListeners(); // 로그인 상태 변경 알림
+//   }
+// }
