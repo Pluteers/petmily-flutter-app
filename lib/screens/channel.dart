a1e7e5d411006173aa078acacba2f8ca63b1fa93
devpark435 <devpark435@gmail.com>
2023-05-09 13:32:35 +0900

    modified(#17) : post data with futurebuilder

--- lib/screens/channel.dart
+++ lib/screens/channel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:petmily/providers/get_post.dart';
 import 'package:petmily/widgets/variable_text.dart';
 
 class ChannelScreen extends StatelessWidget {
@@ -35,119 +36,148 @@ class ChannelScreen extends StatelessWidget {
     final height = MediaQuery.of(context).size.height;
     return Scaffold(
       body: SafeArea(
-        child: Column(children: [
-          Container(
-              margin: const EdgeInsets.only(left: 20),
-              width: width,
-              alignment: Alignment.topLeft,
-              child: VariableText(
-                value: channelId,
-                size: 40,
-                wght: 300,
-                color: dynamicColor.primary,
-              )),
-          Container(
-              width: width,
-              padding: const EdgeInsets.only(left: 20),
-              alignment: Alignment.topLeft,
-              child: VariableText(
-                value: "총 ${channelPostList.length} 건",
-                size: 14,
-                wght: 300,
-                color: dynamicColor.onSurface,
-              )),
-          const SizedBox(
-            height: 30,
-          ),
-          SizedBox(
-            height: height * 0.7,
-            width: width,
-            child: ListView.builder(
-                itemCount: channelPostList.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    margin: const EdgeInsets.all(10),
-                    color: dynamicColor
-                        .surfaceVariant, //? figma surface-container 라고 되어 있는데 dynamicColor에 없는듯
-                    elevation: 0,
-                    child: Column(
-                      children: [
-                        Container(
-                            width: width,
-                            alignment: Alignment.topLeft,
-                            padding: const EdgeInsets.only(left: 10, top: 10),
-                            child: Row(
-                              children: [
-                                const Icon(Icons.person),
-                                Text(channelPostList[index].postWriter),
-                              ],
-                            )),
-                        Container(
-                          margin: const EdgeInsets.all(10),
-                          alignment: Alignment.centerLeft,
-                          width: width,
-                          height: 150,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              // alignment: Alignment.topLeft,
-                              image: AssetImage(channelPostList[index]
-                                  .postImagePath), // 이미지 파일 경로
-                              fit: BoxFit.fitHeight, // 이미지 채우는 방법
-                            ),
-                          ),
-                        ),
-                        Container(
-                          width: width,
-                          alignment: Alignment.topLeft,
-                          padding: const EdgeInsets.only(left: 10, bottom: 10),
-                          child: Text(channelPostList[index].postTitle),
-                        ),
-                        Container(
-                          width: width,
-                          alignment: Alignment.topLeft,
-                          padding: const EdgeInsets.only(left: 10, bottom: 10),
-                          child: Text(channelPostList[index].postSubTitle),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.only(bottom: 10, right: 10),
-                          child: Row(
-                            children: [
-                              const Expanded(
-                                flex: 2,
-                                child: SizedBox(),
-                              ),
-                              Expanded(
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    Text(
-                                        "${channelPostList[index].postFavoriteCount}"),
-                                    const Text("Favorites"),
-                                  ],
-                                ),
-                              ),
-                              Expanded(
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceEvenly,
-                                  children: [
-                                    Text(
-                                        "${channelPostList[index].postCommentCount}"),
-                                    const Text("Comments"),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
+          child: FutureBuilder(
+              future: getPost(channelId),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  /** API 데이터에 체널 이름 없음*/
+                  var channelDataLength = snapshot.data!.data!.length;
+                  return Column(children: [
+                    Container(
+                        margin: const EdgeInsets.only(left: 20),
+                        width: width,
+                        alignment: Alignment.topLeft,
+                        child: VariableText(
+                          value: "$channelId",
+                          size: 40,
+                          wght: 300,
+                          color: dynamicColor.primary,
+                        )),
+                    Container(
+                        width: width,
+                        padding: const EdgeInsets.only(left: 20),
+                        alignment: Alignment.topLeft,
+                        child: VariableText(
+                          value: "총 $channelDataLength건",
+                          size: 14,
+                          wght: 300,
+                          color: dynamicColor.onSurface,
+                        )),
+                    const SizedBox(
+                      height: 30,
                     ),
+                    SizedBox(
+                      height: height * 0.7,
+                      child: ListView.builder(
+                          itemCount: channelDataLength,
+                          itemBuilder: (context, index) {
+                            var postTitle = snapshot.data!.data![index].title;
+                            var postContent =
+                                snapshot.data!.data![index].content;
+                            var postFavorite =
+                                snapshot.data!.data![index].likePost;
+                            var postCreatday = snapshot
+                                .data!.data![index].createDate!
+                                .substring(0, 10);
+                            var postModiday = snapshot
+                                .data!.data![index].lastModifiedDate!
+                                .substring(0, 10);
+                            var postWriter =
+                                snapshot.data!.data![index].nickname;
+                            return Card(
+                              margin: const EdgeInsets.all(10),
+                              color: dynamicColor
+                                  .surfaceVariant, //? figma surface-container 라고 되어 있는데 dynamicColor에 없는듯
+                              elevation: 0,
+                              child: Column(
+                                children: [
+                                  Container(
+                                      width: width,
+                                      alignment: Alignment.topLeft,
+                                      padding: const EdgeInsets.only(
+                                          left: 10, top: 10),
+                                      child: Container(
+                                          alignment: Alignment.topLeft,
+                                          height: 40,
+                                          child: Row(
+                                            children: [
+                                              const Icon(Icons.person),
+                                              Text('$postWriter'),
+                                            ],
+                                          ))),
+                                  Container(
+                                    margin: const EdgeInsets.all(10),
+                                    alignment: Alignment.centerLeft,
+                                    width: width,
+                                    height: 150,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        // alignment: Alignment.topLeft,
+                                        image: AssetImage(channelPostList[index]
+                                            .postImagePath), // 이미지 파일 경로
+                                        fit: BoxFit.fitHeight, // 이미지 채우는 방법
+                                      ),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: width,
+                                    alignment: Alignment.topLeft,
+                                    padding: const EdgeInsets.only(
+                                        left: 10, bottom: 10),
+                                    child: Text('$postTitle'),
+                                  ),
+                                  Container(
+                                    width: width,
+                                    alignment: Alignment.topLeft,
+                                    padding: const EdgeInsets.only(
+                                        left: 10, bottom: 10),
+                                    child: Text('$postContent'),
+                                  ),
+                                  Container(
+                                    padding: const EdgeInsets.only(
+                                        bottom: 10, right: 10),
+                                    child: Row(
+                                      children: [
+                                        const Expanded(
+                                          flex: 2,
+                                          child: SizedBox(),
+                                        ),
+                                        Expanded(
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceEvenly,
+                                            children: [
+                                              Text("$postFavorite"),
+                                              const Text("Favorites"),
+                                            ],
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceEvenly,
+                                            children: [
+                                              Text(
+                                                  "${channelPostList[index].postCommentCount}"),
+                                              const Text("Comments"),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            );
+                          }),
+                    )
+                  ]);
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
                   );
-                }),
-          )
-        ]),
-      ),
+                }
+              })),
       bottomNavigationBar: BottomAppBar(
         height: 80,
         color: dynamicColor.surfaceVariant,
@@ -156,7 +186,9 @@ class ChannelScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         backgroundColor: dynamicColor.secondary,
         elevation: 0,
-        onPressed: () {},
+        onPressed: () {
+          getPost(channelId);
+        },
         tooltip: 'Add',
         child: Icon(
           Icons.add,
@@ -166,6 +198,96 @@ class ChannelScreen extends StatelessWidget {
     );
   }
 }
+
+// Widget postContainer{
+//   return SizedBox(
+//             height: height * 0.7,
+//             width: width,
+//             child: ListView.builder(
+//                 itemCount: channelPostList.length,
+//                 itemBuilder: (context, index) {
+//                   return Card(
+//                     margin: const EdgeInsets.all(10),
+//                     color: dynamicColor
+//                         .surfaceVariant, //? figma surface-container 라고 되어 있는데 dynamicColor에 없는듯
+//                     elevation: 0,
+//                     child: Column(
+//                       children: [
+//                         Container(
+//                             width: width,
+//                             alignment: Alignment.topLeft,
+//                             padding: const EdgeInsets.only(left: 10, top: 10),
+//                             child: Row(
+//                               children: [
+//                                 const Icon(Icons.person),
+//                                 Text(channelPostList[index].postWriter),
+//                               ],
+//                             )),
+//                         Container(
+//                           margin: const EdgeInsets.all(10),
+//                           alignment: Alignment.centerLeft,
+//                           width: width,
+//                           height: 150,
+//                           decoration: BoxDecoration(
+//                             image: DecorationImage(
+//                               // alignment: Alignment.topLeft,
+//                               image: AssetImage(channelPostList[index]
+//                                   .postImagePath), // 이미지 파일 경로
+//                               fit: BoxFit.fitHeight, // 이미지 채우는 방법
+//                             ),
+//                           ),
+//                         ),
+//                         Container(
+//                           width: width,
+//                           alignment: Alignment.topLeft,
+//                           padding: const EdgeInsets.only(left: 10, bottom: 10),
+//                           child: Text(channelPostList[index].postTitle),
+//                         ),
+//                         Container(
+//                           width: width,
+//                           alignment: Alignment.topLeft,
+//                           padding: const EdgeInsets.only(left: 10, bottom: 10),
+//                           child: Text(channelPostList[index].postSubTitle),
+//                         ),
+//                         Container(
+//                           padding: const EdgeInsets.only(bottom: 10, right: 10),
+//                           child: Row(
+//                             children: [
+//                               const Expanded(
+//                                 flex: 2,
+//                                 child: SizedBox(),
+//                               ),
+//                               Expanded(
+//                                 child: Row(
+//                                   mainAxisAlignment:
+//                                       MainAxisAlignment.spaceEvenly,
+//                                   children: [
+//                                     Text(
+//                                         "${channelPostList[index].postFavoriteCount}"),
+//                                     const Text("Favorites"),
+//                                   ],
+//                                 ),
+//                               ),
+//                               Expanded(
+//                                 child: Row(
+//                                   mainAxisAlignment:
+//                                       MainAxisAlignment.spaceEvenly,
+//                                   children: [
+//                                     Text(
+//                                         "${channelPostList[index].postCommentCount}"),
+//                                     const Text("Comments"),
+//                                   ],
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                         )
+//                       ],
+//                     ),
+//                   );
+//                 }),
+//           );
+// }
 
 class PostData {
   final String postWriter;
