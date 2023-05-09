5163d2741a8addf7823032cf0cdc19bb6acb9c51
devpark435 <devpark435@gmail.com>
2023-05-09 12:27:00 +0900

    modified(#17) : modified api request url

--- lib/providers/get_channel.dart
+++ lib/providers/get_channel.dart
@@ -4,11 +4,15 @@ import 'package:dio/dio.dart';
 final dio = Dio();
 Future<GetChannel> getChannel() async {
   var accessToken =
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTY4MzU0ODc4MiwiZW1haWwiOiJ0ZXN0NEBuYXZlci5jb20ifQ.695ZHvXIJzsRet2BKluBhTTVBoqkN1pBmszSOva6xFxa0__7phktJYvobq9WWnARYx57WarddZ6tm2D5LbmU9Q";
-  dio.options.headers["authorization"] = "Bearer $accessToken";
-  var response = await dio.get(
-    ("http://petmily.duckdns.org/post/channel"),
-  );
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTY4NDgxMjI1NiwiZW1haWwiOiJ0ZXN0NEBuYXZlci5jb20ifQ.pEG7RJAy4CK7go9keWwaYDGiIePZm613hp-CLLVA8NucH1QKYs_RWNAenQNf_Nmq4uQB9m8MIJIRet4bk21IHA";
+
+  var response = await dio.get(("http://petmily.duckdns.org/channel"),
+      options: Options(
+        headers: {
+          'Authorization': 'Bearer ' + accessToken,
+          'Content-Type': 'application/json'
+        },
+      ));
 
   final responseData = response.data;
   var resData = responseData;
