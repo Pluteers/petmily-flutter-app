58febf82e24b859e9ae9400e99219670f34ae25b
devpark435 <devpark435@gmail.com>
2023-05-09 12:10:36 +0900

    delete : 필요없는 코드 및 주석 처리 구문 삭제

--- lib/providers/post_channel.dart
+++ lib/providers/post_channel.dart
@@ -10,8 +10,6 @@ class PostChannel {
   static Future<void> postChannel(category) async {
     var accessToken =
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTY4MzU0ODc4MiwiZW1haWwiOiJ0ZXN0NEBuYXZlci5jb20ifQ.695ZHvXIJzsRet2BKluBhTTVBoqkN1pBmszSOva6xFxa0__7phktJYvobq9WWnARYx57WarddZ6tm2D5LbmU9Q";
-    // dio.options.headers["Authorization"] = "Bearer $accessToken";
-    // dio.options.headers["Content-Type"] = "application/json";
 
     var response = await dio.post(
       "http://petmily.duckdns.org/post/channel",
@@ -32,12 +30,3 @@ class PostChannel {
     log('channelPostSuccess : $resData');
   }
 }
-
-
-
-//Header : {   Authorization: Bearer (Access Token필요) }
-
-// {
-// "channelName" :"짱이의 일상2",
-// "categoryId": "1"
-// }
