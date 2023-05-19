dd33b76f8b29bd2d2a1cfb8c1ddf65e48225aa31
yujinkim1 <yujin7621@me.com>
2023-05-19 17:41:36 +0900

    add: Server path

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const domain = "http://petmily.duckdns.org/";
+  static const signin = "login";
+  static const signup = "sign-up";
+  static const kakao = "login/oauth2/code/kakao";
+}
