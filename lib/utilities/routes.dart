1bc79cfc381907f9247fd94228d588e489ca5d47
yujinkim1 <yujin7621@me.com>
2023-05-19 22:58:16 +0900

    add: Routes of go router

---
+++ lib/utilities/routes.dart
@@ -0,0 +1,22 @@
+// import 'package:go_router/go_router.dart';
+
+// import 'package:petmily/screens/initial_screen.dart';
+// import 'package:petmily/screens/local/signin_screen.dart';
+// import 'package:petmily/screens/local/signup_screen.dart';
+
+// final GoRouter router = GoRouter(
+//   routes: <RouteBase>[
+//     GoRoute(
+//       path: "/",
+//       builder: (context, state) => const InitialScreen(),
+//     ),
+//     GoRoute(
+//       path: "/signin",
+//       builder: (context, state) => const SignInScreen(),
+//     ),
+//     GoRoute(
+//       path: "/signup",
+//       builder: (context, state) => const SignUpScreen(),
+//     ),
+//   ],
+// );
