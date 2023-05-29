dd183c13db9bd278a526990e34777f902d80194e
devpark435 <devpark435@gmail.com>
2023-05-29 16:26:44 +0900

    add(#17) : add edit post screen

---
+++ lib/screens/post/edit_post.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditPostScreen extends StatelessWidget {
+  const EditPostScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
