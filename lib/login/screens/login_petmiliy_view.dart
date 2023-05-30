49d6f6dd6b7cccf9c5db19629ab1396bbef6719e
devpark435 <devpark435@gmail.com>
2023-05-30 22:41:41 +0900

    modified : scrollview => safeArea

--- lib/login/screens/login_petmiliy_view.dart
+++ lib/login/screens/login_petmiliy_view.dart
@@ -20,64 +20,66 @@ class _LoginPetmiliyViewState extends State<LoginPetmiliyView> {
   Widget build(BuildContext context) {
     return Consumer<AuthService>(builder: (context, authService, child) {
       return Scaffold(
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Center(
-                child: Text(
-                  "Log in",
-                  style: TextStyle(
-                    fontSize: 24,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Center(
+                  child: Text(
+                    "Log in",
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 32),
+                SizedBox(height: 32),
 
-              /// 이메일
-              TextField(
-                controller: emailController,
-                decoration: InputDecoration(hintText: "e-mail"),
-              ),
+                /// 이메일
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(hintText: "e-mail"),
+                ),
 
-              /// 비밀번호
-              TextField(
-                controller: passwordController,
-                obscureText: true,
-                decoration: InputDecoration(hintText: "password"),
-              ),
-              SizedBox(height: 32),
+                /// 비밀번호
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(hintText: "password"),
+                ),
+                SizedBox(height: 32),
 
-              /// 로그인 버튼
-              ElevatedButton(
-                child: Text("Log in", style: TextStyle(fontSize: 21)),
-                onPressed: () {
-                  authService.signIn(
-                    email: emailController.text,
-                    password: passwordController.text,
-                    onSuccess: () {
-                      // 로그인 성공
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text("로그인 성공"),
-                      ));
+                /// 로그인 버튼
+                ElevatedButton(
+                  child: Text("Log in", style: TextStyle(fontSize: 21)),
+                  onPressed: () {
+                    authService.signIn(
+                      email: emailController.text,
+                      password: passwordController.text,
+                      onSuccess: () {
+                        // 로그인 성공
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text("로그인 성공"),
+                        ));
 
-                      // HomePage로 이동
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => HomeWidget()),
-                      );
-                    },
-                    onError: (err) {
-                      // 에러 발생
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text(err),
-                      ));
-                    },
-                  );
-                },
-              ),
-            ],
+                        // HomePage로 이동
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => HomeWidget()),
+                        );
+                      },
+                      onError: (err) {
+                        // 에러 발생
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text(err),
+                        ));
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       );
