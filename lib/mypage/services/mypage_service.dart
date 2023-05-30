b1999fa45da6d5e6e464811cc1c1fb65813acfd7
sub101 <subin10@nate.com>
2023-05-30 20:53:36 +0900

    토큰 발급 실패

--- lib/mypage/services/mypage_service.dart
+++ lib/mypage/services/mypage_service.dart
@@ -1,31 +1,42 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MyPageScreen extends StatefulWidget {
-  final String accessToken;
-
-  MyPageScreen({required this.accessToken});
-
   @override
   _MyPageScreenState createState() => _MyPageScreenState();
 }
 
 class _MyPageScreenState extends State<MyPageScreen> {
   late Future<Map<String, dynamic>> _fetchMyPageInfo;
+  late String _accessToken;
 
   @override
   void initState() {
     super.initState();
 
-    _fetchMyPageInfo = _fetchMyPageInfoFromApi();
+    getToken().then((String? token) {
+      if (token != null) {
+        setState(() {
+          _accessToken = token;
+          _fetchMyPageInfo = _fetchMyPageInfoFromApi();
+        });
+      }
+    });
+  }
+
+  Future<String?> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token');
   }
 
   Future<Map<String, dynamic>> _fetchMyPageInfoFromApi() async {
     String apiUrl = "http://petmily.duckdns.org/user/info";
 
     Map<String, String> headers = {
-      'Authorization': 'Bearer ${widget.accessToken}',
+      'Authorization': 'Bearer $_accessToken',
     };
 
     http.Response response =
@@ -35,6 +46,7 @@ class _MyPageScreenState extends State<MyPageScreen> {
       Map<String, dynamic> data = json.decode(response.body);
       return data;
     } else {
+      log("마이페이지에러 ${response.bodyBytes}");
       throw Exception('Failed to fetch data from API');
     }
   }
@@ -57,19 +69,15 @@ class _MyPageScreenState extends State<MyPageScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text(
-                      'Name: ${myPageInfo['name']}',
+                      'Name: ${myPageInfo['email']}',
                       style: TextStyle(fontSize: 18.0),
                     ),
                     SizedBox(height: 10.0),
                     Text(
-                      'Email: ${myPageInfo['email']}',
+                      'Email: ${myPageInfo['nickname']}',
                       style: TextStyle(fontSize: 18.0),
                     ),
                     SizedBox(height: 10.0),
-                    Text(
-                      'Phone: ${myPageInfo['phone']}',
-                      style: TextStyle(fontSize: 18.0),
-                    ),
                   ],
                 ),
               );
@@ -92,3 +100,98 @@ class _MyPageScreenState extends State<MyPageScreen> {
     );
   }
 }
+
+// import 'dart:convert';
+// import 'dart:developer';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+
+// class MyPageScreen extends StatefulWidget {
+//   final String accessToken;
+
+//   MyPageScreen({required this.accessToken});
+
+//   @override
+//   _MyPageScreenState createState() => _MyPageScreenState();
+// }
+
+// class _MyPageScreenState extends State<MyPageScreen> {
+//   late Future<Map<String, dynamic>> _fetchMyPageInfo;
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     _fetchMyPageInfo = _fetchMyPageInfoFromApi();
+//   }
+
+//   Future<Map<String, dynamic>> _fetchMyPageInfoFromApi() async {
+//     String apiUrl = "http://petmily.duckdns.org/user/info";
+
+//     Map<String, String> headers = {
+//       'Authorization':
+//           'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTY4NjA0MjgzOSwiZW1haWwiOiJ0ZXN0NEBuYXZlci5jb20ifQ.svOFWFpDQvIT0XPqf4D5fvBFIqULVE5hL_LaaNl3bC1AQ103lz9xtCofr_kbufXMi7CbNtyPG9feEOTUTbLIsw',
+//     };
+
+//     http.Response response =
+//         await http.get(Uri.parse(apiUrl), headers: headers);
+
+//     if (response.statusCode == 200) {
+//       Map<String, dynamic> data = json.decode(response.body);
+//       return data;
+//     } else {
+//       log("마이페이지에러 ${response.bodyBytes}");
+//       throw Exception('Failed to fetch data from API');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('My Page'),
+//       ),
+//       body: FutureBuilder<Map<String, dynamic>>(
+//         future: _fetchMyPageInfo,
+//         builder: (BuildContext context,
+//             AsyncSnapshot<Map<String, dynamic>> snapshot) {
+//           if (snapshot.connectionState == ConnectionState.done) {
+//             if (snapshot.hasData) {
+//               Map<String, dynamic> myPageInfo = snapshot.data!;
+//               return Center(
+//                 child: Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: <Widget>[
+//                     Text(
+//                       'Name: ${myPageInfo['email']}',
+//                       style: TextStyle(fontSize: 18.0),
+//                     ),
+//                     SizedBox(height: 10.0),
+//                     Text(
+//                       'Email: ${myPageInfo['nickname']}',
+//                       style: TextStyle(fontSize: 18.0),
+//                     ),
+//                     SizedBox(height: 10.0),
+//                   ],
+//                 ),
+//               );
+//             } else if (snapshot.hasError) {
+//               return Center(
+//                 child: Text('Failed to fetch data from API'),
+//               );
+//             } else {
+//               return Center(
+//                 child: CircularProgressIndicator(),
+//               );
+//             }
+//           } else {
+//             return Center(
+//               child: CircularProgressIndicator(),
+//             );
+//           }
+//         },
+//       ),
+//     );
+//   }
+// }
+
--- lib/signup/services/auth_service.dart
+++ lib/signup/services/auth_service.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
 class AuthService extends ChangeNotifier {
   final dio = Dio();
@@ -42,7 +46,6 @@ class AuthService extends ChangeNotifier {
     required Function() onSuccess,
     required Function(String err) onError,
   }) async {
-    // 로그인
     if (email.isEmpty) {
       onError('이메일을 입력해주세요.');
       return;
@@ -51,12 +54,51 @@ class AuthService extends ChangeNotifier {
       return;
     }
 
+    var headers = {'Content-Type': 'application/json'};
+    var request =
+        http.Request('POST', Uri.parse('http://petmily.duckdns.org/login'));
+    request.body =
+        json.encode({"email": "test2@naver.com", "password": "test123"});
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      log(await response.stream.bytesToString());
+    } else {
+      log(response.reasonPhrase!);
+    }
+
     // 로그인 시도
-    final url = 'http://petmily.duckdns.org/login';
-    final response = await dio.post(url, data: {
-      'email': email,
-      'password': password,
-    });
-    onSuccess();
+    // final url = 'http://petmily.duckdns.org/login';
+    // final response = await dio.post(url, data: {
+    //   'email': email,
+    //   'password': password,
+    // });
+    // onSuccess();
+
+    //   final url = 'http://petmily.duckdns.org/login';
+    //   final response = await http.post(Uri.parse(url), headers: {
+    //     "Content-Type": "application/json"
+    //   }, body: {
+    //     'email': email,
+    //     'password': password,
+    //   });
+
+    //   if (response.statusCode == 200) {
+    //     final responseData = jsonDecode(response.body);
+    //     final token = responseData['Authorization'];
+
+    //     final prefs = await SharedPreferences.getInstance();
+    //     await prefs.setString('token', token);
+    //     String? check = prefs.getString('token');
+    //     //log('prefs에 저장된 토큰: $check');
+    //     debugPrint('prefs에 저장된 토큰 : $check');
+
+    //     onSuccess();
+    //   } else {
+    //     onError('로그인에 실패했습니다.');
+    //     debugPrint('else ${response.statusCode}');
+    //   }
   }
 }
