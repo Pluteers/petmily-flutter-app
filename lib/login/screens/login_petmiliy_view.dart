9893190af2c6791d08975c64b952970c5d93d826
devpark435 <devpark435@gmail.com>
2023-06-05 14:00:12 +0900

    modified : Login screen UI refactoring

--- lib/login/screens/login_petmiliy_view.dart
+++ lib/login/screens/login_petmiliy_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:petmily/login/screens/login_view.dart';
 import 'package:petmily/main.dart';
 import 'package:petmily/signup/services/auth_service.dart';
+import 'package:petmily/widgets/variable_text.dart';
 import 'package:provider/provider.dart';
 
 class LoginPetmiliyView extends StatefulWidget {
@@ -18,67 +19,179 @@ class _LoginPetmiliyViewState extends State<LoginPetmiliyView> {
 
   @override
   Widget build(BuildContext context) {
+    final dynamicColor = Theme.of(context).colorScheme;
+    final width = MediaQuery.of(context).size.width;
     return Consumer<AuthService>(builder: (context, authService, child) {
       return Scaffold(
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Center(
-                  child: Text(
-                    "Log in",
-                    style: TextStyle(
-                      fontSize: 24,
+            child: SizedBox(
+              width: width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  VariableText(
+                    value: "Petmiliy",
+                    size: 45,
+                    wght: 900,
+                    color: dynamicColor.primary,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: 30,
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        TyperAnimatedText(
+                          '우리가 만드는 반려동물 캠퍼스',
+                          textStyle: TextStyle(
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.w400,
+                            color: dynamicColor.primary,
+                          ),
+                          speed: const Duration(milliseconds: 100),
+                        ),
+                      ],
+                      totalRepeatCount: 1,
                     ),
                   ),
-                ),
-                SizedBox(height: 32),
 
-                /// 이메일
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(hintText: "e-mail"),
-                ),
+                  const SizedBox(
+                    height: 40.0,
+                  ),
 
-                /// 비밀번호
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(hintText: "password"),
-                ),
-                SizedBox(height: 32),
+                  /// ID 입력 Form
+                  SizedBox(
+                    width: width * .9,
+                    child: TextFormField(
+                      controller: emailController,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        hintText: '이메일 형식 아이디를 입력하세요.',
+                        labelText: '아이디',
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.close),
+                          onPressed: () {
+                            emailController.clear();
+                          },
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25),
+                            borderSide: BorderSide(
+                                width: 1, color: dynamicColor.primary)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
 
-                /// 로그인 버튼
-                ElevatedButton(
-                  child: Text("Log in", style: TextStyle(fontSize: 21)),
-                  onPressed: () {
-                    authService.signIn(
-                      email: emailController.text,
-                      password: passwordController.text,
-                      onSuccess: () {
-                        // 로그인 성공
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text("로그인 성공"),
-                        ));
+                  /// PW 입력 Form
+                  SizedBox(
+                    width: width * .9,
+                    child: TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: '비밀번호',
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.close),
+                          onPressed: () {
+                            passwordController.clear();
+                          },
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25),
+                            borderSide: BorderSide(
+                                width: 1, color: dynamicColor.primary)),
+                      ),
+                    ),
+                  ),
 
-                        // HomePage로 이동
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(builder: (context) => HomeWidget()),
-                        );
-                      },
-                      onError: (err) {
-                        // 에러 발생
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text(err),
-                        ));
-                      },
-                    );
-                  },
-                ),
-              ],
+                  SizedBox(height: 32),
+                  GestureDetector(
+                    child: Container(
+                        width: width * .5,
+                        height: 40,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: dynamicColor.primary.withOpacity(0.4),
+                                blurRadius: 5.0,
+                                spreadRadius: 0.0,
+                                offset: const Offset(0, 5),
+                              )
+                            ],
+                            borderRadius: BorderRadius.circular(25)),
+                        child: VariableText(
+                          value: "로그인",
+                          size: 19,
+                          wght: 400,
+                          color: dynamicColor.primary,
+                        )),
+                    onTap: () {
+                      // 회원가입 화면으로 이동
+                      authService.signIn(
+                        email: emailController.text,
+                        password: passwordController.text,
+                        onSuccess: () {
+                          // 로그인 성공
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("로그인 성공"),
+                          ));
+
+                          // HomePage로 이동
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeWidget()),
+                          );
+                        },
+                        onError: (err) {
+                          // 에러 발생
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(err),
+                          ));
+                        },
+                      );
+                    },
+                  ),
+
+                  /// 로그인 버튼
+                  // ElevatedButton(
+                  //   child: Text("Log in", style: TextStyle(fontSize: 21)),
+                  //   onPressed: () {
+                  //     authService.signIn(
+                  //       email: emailController.text,
+                  //       password: passwordController.text,
+                  //       onSuccess: () {
+                  //         // 로그인 성공
+                  //         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  //           content: Text("로그인 성공"),
+                  //         ));
+
+                  //         // HomePage로 이동
+                  //         Navigator.pushReplacement(
+                  //           context,
+                  //           MaterialPageRoute(builder: (context) => HomeWidget()),
+                  //         );
+                  //       },
+                  //       onError: (err) {
+                  //         // 에러 발생
+                  //         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  //           content: Text(err),
+                  //         ));
+                  //       },
+                  //     );
+                  //   },
+                  // ),
+                ],
+              ),
             ),
           ),
         ),
