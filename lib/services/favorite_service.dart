5a1d639f7c37eeed8a58c5b1287ba50075b897c1
devpark435 <devpark435@gmail.com>
2023-06-08 18:07:33 +0900

    modified : favorite service type favoritelistmodel

--- lib/services/favorite_service.dart
+++ lib/services/favorite_service.dart
@@ -1,16 +1,18 @@
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:petmily/models/favorite_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final dio = Dio();
 
 class FavoriteService {
   // http://petmily.duckdns.org/bookmark
-  Future<void> userFavoriteList() async {
+  Future<FavoriteListModel?> userFavoriteList() async {
     const url = "http://petmily.duckdns.org/bookmark";
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     final String? accessToken = prefs.getString('token');
+    FavoriteListModel? favoriteListModel;
 
     try {
       final response = await dio.get((url),
@@ -24,11 +26,16 @@ class FavoriteService {
         final responseData = response.data;
 
         log('FavoriteList Success : $responseData');
+        final favoriteListData = FavoriteListModel.fromJson(responseData);
+        favoriteListModel = favoriteListData;
+        return favoriteListModel;
       } else {
         log("${response.statusCode}");
+        return favoriteListModel;
       }
     } catch (e) {
       log("$e");
+      return favoriteListModel;
     }
   }
 }
