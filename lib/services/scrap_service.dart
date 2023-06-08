f2977f67eb5ddeceec5d919bc5e88d48c2cf6022
devpark435 <devpark435@gmail.com>
2023-06-08 14:36:45 +0900

    add : add scrap list model return

--- lib/services/scrap_service.dart
+++ lib/services/scrap_service.dart
@@ -1,17 +1,19 @@
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:petmily/models/scrap_model.dart';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 final dio = Dio();
 
 class ScrapService {
   //http://petmily.duckdns.org/scrap/list
-  Future<void> userScrapList() async {
-    const url = "http://petmily.duckdns.org/bookmark";
+  Future<ScrapListModel?> userScrapList() async {
+    const url = "http://petmily.duckdns.org/scrap/list";
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     final String? accessToken = prefs.getString('token');
-
+    ScrapListModel? scrapModel;
     try {
       final response = await dio.get((url),
           options: Options(
@@ -24,11 +26,16 @@ class ScrapService {
         final responseData = response.data;
 
         log('ScrapList Success : $responseData');
+        final userScrapData = ScrapListModel.fromJson(responseData);
+        scrapModel = userScrapData;
+        return scrapModel;
       } else {
         log("${response.statusCode}");
+        return scrapModel;
       }
     } catch (e) {
       log("$e");
+      return scrapModel;
     }
   }
 }
