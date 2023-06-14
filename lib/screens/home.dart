6795b05be044041af1701e5f23bd609999cdb279
devpark435 <devpark435@gmail.com>
2023-06-14 22:44:54 +0900

    add : lottie category image

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:lottie/lottie.dart';
 import 'package:petmily/providers/get_channel.dart';
 import 'package:petmily/screens/post/post_list.dart';
 
@@ -26,6 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
       create: (context) => GetChannelData(),
       child: Scaffold(
         appBar: AppBar(
+          automaticallyImplyLeading: false,
           title: Padding(
             padding: const EdgeInsets.only(left: 10.0),
             child: Container(
@@ -40,7 +42,6 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
-<<<<<<< HEAD
         ),
         backgroundColor: dynamicColor.background,
         body: const HomeGrideView(),
@@ -59,10 +60,6 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Icon(
             Icons.add,
             color: dynamicColor.onSecondary,
-=======
-          HomeGrideView(
-            categoryDataList: categoryDataList,
->>>>>>> main
           ),
         ),
       ),
@@ -143,7 +140,6 @@ void addChannel(_) async {
       });
 }
 
-<<<<<<< HEAD
 // 메인화면 그리드 뷰
 class HomeGrideView extends StatefulWidget {
   const HomeGrideView({super.key});
@@ -157,154 +153,116 @@ class _HomeGrideViewState extends State<HomeGrideView> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Provider.of<GetChannelData>(context, listen: false).getChannel();
   }
-=======
-//메인화면 그리드 뷰
-class HomeGrideView extends StatelessWidget {
-  const HomeGrideView({super.key, required this.categoryDataList});
-  final dynamic categoryDataList;
->>>>>>> main
 
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     final dynamicColor = Theme.of(context).colorScheme;
-<<<<<<< HEAD
 
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: SizedBox(
         width: width,
-        child: Consumer<GetChannelData>(builder: (context, getChannel, child) {
-          if (getChannel.data.isEmpty) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            return GridView.builder(
-              itemCount: getChannel.data.length,
-              itemBuilder: (context, index) {
-                var channelTitle = getChannel.data[index].channelName;
-                var channelId = getChannel.data[index].channelId;
-                var categoryId = getChannel.data[index].categoryId;
-                var categoryIcon;
-                if (categoryId == 1) {
-                  categoryIcon = FaIcon(
-                    FontAwesomeIcons.dog,
-                    color: dynamicColor.onPrimary,
-                  );
-                } else {
-                  categoryIcon = FaIcon(
-                    FontAwesomeIcons.cat,
-                    color: dynamicColor.secondary,
-                  );
-                }
-                return Container(
-                  decoration: BoxDecoration(
-                      color: dynamicColor.primaryContainer,
-                      borderRadius: BorderRadius.circular(15)),
-                  child: Column(
-                    children: [
-                      GestureDetector(
-                        child: Container(
-                            width: width,
-                            height: 50,
-                            alignment: Alignment.center,
-                            padding: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                                color: dynamicColor.primary,
-                                borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(15),
-                                  topRight: Radius.circular(15),
-                                )),
-                            child: VariableText(
-                              value: "$channelTitle",
-                              size: 18,
-                              color: dynamicColor.onPrimary,
-                              wght: 500,
-                            )),
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ChannelScreen(
-                                        channelId: channelId,
-                                        channelTitle: channelTitle,
-                                      )));
-                        },
+        child: FutureBuilder(
+            future: GetChannelData().getChannel(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (snapshot.connectionState == ConnectionState.done) {
+                return GridView.builder(
+                  itemCount: snapshot.data!.data!.length,
+                  itemBuilder: (context, index) {
+                    var channelTitle = snapshot.data!.data![index].channelName;
+                    var channelId = snapshot.data!.data![index].channelId;
+                    var categoryId = snapshot.data!.data![index].categoryId;
+                    // var categoryIcon;
+                    // if (categoryId == 1) {
+                    //   categoryIcon = FaIcon(
+                    //     FontAwesomeIcons.dog,
+                    //     color: dynamicColor.onPrimary,
+                    //   );
+                    // } else {
+                    //   categoryIcon = FaIcon(
+                    //     FontAwesomeIcons.cat,
+                    //     color: dynamicColor.secondary,
+                    //   );
+                    // }
+                    return InkWell(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: dynamicColor.primaryContainer,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Column(
+                          children: [
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                  width: width,
+                                  height: 50,
+                                  alignment: Alignment.center,
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                      color: dynamicColor.primary,
+                                      borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(15),
+                                        topRight: Radius.circular(15),
+                                      )),
+                                  child: VariableText(
+                                    value: "$channelTitle",
+                                    size: 18,
+                                    color: dynamicColor.onPrimary,
+                                    wght: 500,
+                                  )),
+                            ),
+                            categoryId == 1
+                                ? Expanded(
+                                    flex: 2,
+                                    child: Lottie.network(
+                                        "https://assets4.lottiefiles.com/packages/lf20_OT15QW.json"),
+                                  )
+                                : Expanded(
+                                    flex: 2,
+                                    child: Lottie.network(
+                                        "https://assets9.lottiefiles.com/packages/lf20_syqnfe7c.json"),
+                                  )
+                            // Container(
+                            //   width: width,
+                            //   alignment: Alignment.bottomRight,
+                            //   margin: EdgeInsets.only(bottom: 10, right: 10),
+                            //   child: categoryIcon,
+                            // )
+                          ],
+                        ),
                       ),
-                      SizedBox(
-                        height: 120,
-                      ),
-                      Container(
-                        width: width,
-                        alignment: Alignment.bottomRight,
-                        margin: EdgeInsets.only(bottom: 10, right: 10),
-                        child: categoryIcon,
-                      )
-                    ],
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ChannelScreen(
+                                      channelId: channelId,
+                                      channelTitle: channelTitle,
+                                    )));
+                      },
+                    );
+                  },
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
+                    childAspectRatio: 1 / 1.3, //item 의 가로 1, 세로 1.5 의 비율
+                    mainAxisSpacing: 10, //수평 Padding
+                    crossAxisSpacing: 10, //수직 Padding
                   ),
                 );
-=======
-    return SizedBox(
-      height: height * 0.7,
-      child: GridView.count(
-        crossAxisCount: 2, //한 줄에 들어가는 컨텐츠 숫자
-        children: List.generate(categoryDataList.length, (index) {
-          return Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: GestureDetector(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  image: DecorationImage(
-                    image: AssetImage(
-                        categoryDataList[index].imagePath), // 이미지 파일 경로
-                    fit: BoxFit.cover, // 이미지 채우는 방법
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                        width: width,
-                        padding: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                          borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(15),
-                            topRight: Radius.circular(15),
-                          ),
-                          color: dynamicColor.primary.withOpacity(0.9),
-                        ),
-                        child: VariableText(
-                          value: categoryDataList[index].name,
-                          size: 18,
-                          color: dynamicColor.onPrimary,
-                          wght: 500,
-                        )),
-                  ],
-                ),
-              ),
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ChannelScreen(
-                              channelId: categoryDataList[index].name,
-                            )));
->>>>>>> main
-              },
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
-                childAspectRatio: 1 / 1.3, //item 의 가로 1, 세로 1.5 의 비율
-                mainAxisSpacing: 10, //수평 Padding
-                crossAxisSpacing: 10, //수직 Padding
-              ),
-            );
-          }
-        }),
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            }),
       ),
     );
   }
