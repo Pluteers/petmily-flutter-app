1ce2e9d63ce3a155f4b5cf7f038c45a33e32db62
yujinkim1 <yujin7621@me.com>
2023-06-15 21:37:22 +0900

    refactor(package): #24 Removed unused packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:dynamic_color/dynamic_color.dart';
 
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
-import 'package:petmily/screens/initial_screen.dart';
 import 'package:petmily/screens/splash.dart';
-
 import 'package:petmily/utilities/dynamic_theme.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   KakaoSdk.init(nativeAppKey: 'c906dc51f4a859838fa4b82caae37a41');
